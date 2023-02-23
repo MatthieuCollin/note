90d740239109ca0b2bd42c3dcca8a6f17aaf63ff
steffen76600 <steffenpain76600@gmail.com>
2023-02-23 16:40:30 +0100

    debut page note eleve
    debut page note eleve

---
+++ src/Controller/NoteEleveController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NoteEleveController extends AbstractController
+{
+    #[Route('/note/eleve', name: 'app_note_eleve')]
+    public function index(): Response
+    {
+        return $this->render('note_eleve/index.html.twig', [
+            'controller_name' => 'NoteEleveController',
+        ]);
+    }
+}
