a967a728b2decd81688b2283a8e00e9263c810dc
steffen76600 <steffenpain76600@gmail.com>
2023-02-24 14:41:09 +0100

    creation page programe eleve

---
+++ src/Controller/ProgrameEleveController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProgrameEleveController extends AbstractController
+{
+    #[Route('/programe/eleve', name: 'app_programe_eleve')]
+    public function index(): Response
+    {
+        return $this->render('programe_eleve/index.html.twig', [
+            'controller_name' => 'ProgrameEleveController',
+        ]);
+    }
+}
