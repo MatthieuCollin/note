6b3c3b121e644649457e0c33d927c30399062a3f
Nicolas <125441010+Nidabdo@users.noreply.github.com>
2023-05-29 01:57:52 +0200

    classe modif casi fini + ajotu admin fini

--- src/Form/ClasseType.php
+++ src/Form/ClasseType.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType; 
 
 class ClasseType extends AbstractType
 {
